5cc1faaa431c6cf3d52d39daa9e82408801188e9
umerrana <umerranavvork@gmail.com>
2019-01-28 03:10:51 -0800

    'umer'

---
+++ src/js/components/Footer.jsx
@@ -0,0 +1,10 @@
+import React, {Component} from 'react';
+class footer extends Component{
+    render (){
+        return(
+            <div>
+                Umer
+            </div>
+        )
+    }
+}
