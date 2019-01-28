9a1a93582d8c0df70c62b45bfd38805aeb00ed3f
Your Name <you@example.com>
2019-01-28 16:05:16 +0500

    Header

--- src/js/components/App.jsx
+++ src/js/components/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
+import Hero from './Hero.jsx';
 
 class App extends React.Component {
   render() {
     return (
       <div>
         <div>hello</div>
+        <Hero/>
       </div>
     );
   }
---
+++ src/js/components/Hero.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class Hero extends Component {
+  render() {
+    return (
+      <div>
+           <h1>This is Hero Section</h1>
+      </div>
+    )
+  }
+}
+
+export default Hero;
