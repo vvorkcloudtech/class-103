6c6c00f13cc5855bc15c4c26174143b5a2a6a9bd
VvorkCloudTechnologies <admin@vvorkcloud.com>
2019-01-28 15:57:59 +0500

    header added

--- src/js/components/App.jsx
+++ src/js/components/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
+import Header from "./Header";
 
 class App extends React.Component {
   render() {
     return (
       <div>
         <div>hello</div>
+        <Header />
       </div>
     );
   }
