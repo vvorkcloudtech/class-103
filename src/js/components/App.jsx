2210fe752b99d977499e613ad26399ff66223a8f
Muhammad Adil <adil.muhammad195@gmail.com>
2019-01-28 15:58:18 +0500

    lavaris edit

--- src/js/components/App.jsx
+++ src/js/components/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
+import Lavaris from './Lavaris.jsx';
 
 class App extends React.Component {
   render() {
     return (
       <div>
         <div>hello</div>
+        <Lavaris/>
       </div>
     );
   }
---
+++ src/js/components/Lavaris.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Lavaris extends Component {
+  render() {
+    return (
+      <div>
+        Lavaris Component
+      </div>
+    )
+  }
+}
