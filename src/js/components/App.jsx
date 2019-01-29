1f67e066bd47fc5f7fef268b6aafae722662f9d7
hamzabutt32 <hamza.butt32@hotmail.com>
2019-01-29 17:17:45 +0500

    error removed

--- src/js/components/App.jsx
+++ src/js/components/App.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import Hero from "./Hero.jsx";
 import Header from "./Header.jsx";
 import Services from "./services.jsx";
-import Nav from './navigation'
+import Nav from './navigation';
 
 import Header from "./Header";
 import Services from "./services";
